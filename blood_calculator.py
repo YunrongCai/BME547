99ee1c0f819437d4a424bd53c4cd8530ac21e39e
Yunrong <eric1126@vip.qq.com>
2023-01-20 12:41:00 -0500

    Create a blood calculator

---
+++ blood_calculator.py
@@ -0,0 +1,11 @@
+def interface():
+    print("Blood calculator")
+    keep = True
+    while keep :
+        print("Options:")
+        print("9 - Quit")
+        choice = input("Select an option:")
+        if choice =="9":
+            keep = False
+    print('Program ending')
+interface()
