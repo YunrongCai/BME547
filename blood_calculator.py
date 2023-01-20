791bb52126572f9a28cfd6516f2e79bbca62faf9
Yunrong <eric1126@vip.qq.com>
2023-01-20 12:46:54 -0500

    Classify HDL result

--- blood_calculator.py
+++ blood_calculator.py
@@ -8,4 +8,18 @@ def interface():
         if choice =="9":
             keep = False
     print('Program ending')
+
+
+def HDL_input():
+    HDL_value = input("Enter the HDL result:")
+    HDL_value = int(HDL_value)
+    return  HDL_value
+def HDL_analysis(HDL_int):
+    if HDL_int >= 60:
+        answer = "Normal"
+    elif 40 <= HDL_int < 60:
+        answer = "Borderline Low"
+    else:
+        answer = "Low"
+    return answer
 interface()
