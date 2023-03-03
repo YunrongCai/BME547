a355f893cb88ef93f20e1714cb2a962dd6d8e643
Yunrong <eric1126@vip.qq.com>
2023-03-03 13:15:41 -0500

    Class work on Mar 3 2023

---
+++ health_db_server.py
@@ -0,0 +1,11 @@
+"""
+Database = Dictionary
+keys -> ids for the patients
+value: dict
+
+{
+1:{"id":1, "name":"David", "bloodtype":"O+"},
+2:{"id":2, "name":"David", "bloodtype":"O-"},
+3:{"id":3, "name":"David", "bloodtype":"O+", "test":[]}
+}
+"""
