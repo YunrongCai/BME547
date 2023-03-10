bcd52c9b8c3eb3f10b4b24ded490589b30dbfa64
Yunrong <eric1126@vip.qq.com>
2023-03-10 13:00:27 -0500

    "Changed health db code"

--- health_db_client.py
+++ health_db_client.py
@@ -1,10 +1,25 @@
 import requests
 
-server = " http://127.0.0.1:5000"
+server = "http://127.0.0.1:5000"
 
-patient = {"id":1, "name": "Yunrong", "blood_type": "O+", "test": []}
-r = requests.post(server+'/new_patient', json=patient)
-patient = {"id":1, "test_name": "Blood", "test_result": 1}
-r = requests.post(server+'/add_test', json=patient)
+patient = {"id": 1, "name": "David", "blood_type": "O+"}
+r = requests.post(server + "/new_patient", json=patient)
 print(r.status_code)
 print(r.text)
+
+patient = {"mrn": 2, "name": "John", "blood_type": "A+"}
+r = requests.post(server + "/new_patient", json=patient)
+print(r.status_code)
+print(r.text)
+
+data = {"id": 1, "test_name": "HDL", "test_result": 150}
+r = requests.post(server + "/add_test", json=data)
+print(r.status_code)
+print(r.text)
+
+r = requests.get(server + "/get_results/1")
+print(r.status_code)
+print(r.text)
+answer = r.json()
+for t in answer:
+    print(t)
